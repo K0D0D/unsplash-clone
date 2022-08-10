419b724e7c00eca42e514b9ead6bbc52315f99ed
K0D0D <kovach.daniil.js@gmail.com>
2022-08-10 15:43:06 +0200

    added useScrollToTop hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import useScrollToTop from "./hooks/useScrollToTop";
 import HomePage from "./pages/HomePage/HomePage";
 import TopicPage from "./pages/TopicPage/TopicPage";
 
-const App = () => (
-	<>
-		<Header />
-		<Routes>
-			<Route path="/" element={<HomePage />} />
-			<Route path="/t/:slug" element={<TopicPage />} />
-			<Route path="*" element={<Navigate to="/" />} />
-		</Routes>
-		<Footer />
-	</>
-);
+const App = () => {
+	const { pathname } = useLocation();
+
+	useScrollToTop([pathname]);
+
+	return (
+		<>
+			<Header />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/t/:slug" element={<TopicPage />} />
+				<Route path="*" element={<Navigate to="/" />} />
+			</Routes>
+			<Footer />
+		</>
+	);
+};
 
 export default App;
---
+++ src/hooks/useScrollToTop.ts
@@ -0,0 +1,9 @@
+import { useLayoutEffect } from "react";
+
+const useScrollToTop = (deps: any[]) => {
+	useLayoutEffect(() => {
+		window.scrollTo(0, 0);
+	}, [...deps]);
+};
+
+export default useScrollToTop;
